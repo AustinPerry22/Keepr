aab1a2ef118baf36a1b6431405defb75d744210e
Austin <austinjperry22@gmail.com>
2023-10-20 11:26:42 -0600

    tables made

--- Final/dbSetup.sql
+++ Final/dbSetup.sql
@@ -1,8 +1,44 @@
-CREATE TABLE IF NOT EXISTS accounts(
-  id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
-  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
-  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
-  name varchar(255) COMMENT 'User Name',
-  email varchar(255) COMMENT 'User Email',
-  picture varchar(255) COMMENT 'User Picture'
-) default charset utf8 COMMENT '';
+CREATE TABLE
+    IF NOT EXISTS accounts(
+        id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
+        createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+        updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+        name varchar(255) COMMENT 'User Name',
+        email varchar(255) COMMENT 'User Email',
+        picture varchar(255) COMMENT 'User Picture'
+    ) default charset utf8mb4 COMMENT '';
+
+CREATE TABLE
+    IF NOT EXISTS keeps(
+        id INT NOT NULL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        description VARCHAR(1000) NOT NULL,
+        img VARCHAR(2500) NOT NULL,
+        views INT NOT NULL,
+        creatorId VARCHAR(255) NOT NULL,
+        FOREIGN KEY (creatorId) REFERENCES accounts (id) ON DELETE CASCADE
+    ) default charset utf8mb4 COMMENT '';
+
+CREATE TABLE
+    IF NOT EXISTS vaults(
+        id INT NOT NULL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        description VARCHAR(1000) NOT NULL,
+        img VARCHAR(2500) NOT NULL,
+        isPrivate BOOLEAN NOT NULL,
+        creatorId VARCHAR(255) NOT NULL,
+        FOREIGN KEY(creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+    ) default charset utf8mb4;
+
+CREATE TABLE
+    IF NOT EXISTS vaultKeeps(
+        id INT NOT NULL PRIMARY KEY,
+        creatorId VARCHAR(255) NOT NULL,
+        vaultId INT NOT NULL,
+        keepId INT NOT NULL,
+        FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE,
+        FOREIGN KEY (vaultId) REFERENCES vaults(id) ON DELETE CASCADE,
+        FOREIGN KEY (keepId) REFERENCES keeps(id) ON DELETE CASCADE
+    ) default charset utf8mb4;
+
+DROP TABLE vaults 
