4b484bd3a9a8b7b533e904e60722a392156bfa81
Austin <austinjperry22@gmail.com>
2023-10-22 16:42:31 -0600

    edit so account has cover img

--- Final.client/src/models/Account.js
+++ Final.client/src/models/Account.js
@@ -4,6 +4,7 @@ export class Profile {
     this.id = data.id
     this.name = data.name
     this.picture = data.picture
+    this.coverImg = data.coverImg ? data.coverImg : "https://images.unsplash.com/photo-1487088678257-3a541e6e3922?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Ymxhbmt8ZW58MHx8MHx8fDA%3D"
   }
 }
 
--- Final/dbSetup.sql
+++ Final/dbSetup.sql
@@ -1,3 +1,5 @@
+-- Active: 1696955322374@@SG-Sandbox-7899-mysql-master.servers.mongodirector.com@3306@final
+
 CREATE TABLE
     IF NOT EXISTS accounts(
         id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
@@ -5,7 +7,8 @@ CREATE TABLE
         updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
         name varchar(255) COMMENT 'User Name',
         email varchar(255) COMMENT 'User Email',
-        picture varchar(255) COMMENT 'User Picture'
+        picture varchar(255) COMMENT 'User Picture',
+        coverImg VARCHAR(2000)
     ) default charset utf8mb4 COMMENT '';
 
 CREATE TABLE
